2f55a47649b4d3f91c7725c8f1e3bf33714b3a55
Eric Johnston <emjohn612@gmail.com>
2023-10-31 16:53:27 -0400

    add mock data

---
+++ src/data/Workouts.ts
@@ -0,0 +1,56 @@
+const Workouts = [
+  {
+    id: 1,
+    name: 'Dumbbell Bicep Curl',
+    how_to:
+      'To perform a bicep curl, stand with your feet shoulder-width apart and hold a dumbbell in each hand with your arms fully extended, palms facing forward. Keeping your upper arms stationary, exhale as you curl the weights while contracting your biceps, then inhale as you slowly lower the dumbbells back to the starting position.',
+    muscle_group: 'Arm',
+    video_url: 'https://www.youtube.com/watch?v=ykJmrZ5v0Oo',
+    image_url: require('../assets/bicep-curl.jpg'),
+  },
+  {
+    id: 2,
+    name: 'Lat Pull Down (Wide Grip)',
+    how_to:
+      'To do a lat pulldown, sit at a lat pulldown machine with your thighs secured under the leg pads. Grasp the bar with a wide overhand grip, and as you exhale, pull the bar down to your upper chest while keeping your back straight, then inhale as you slowly release it back up to the starting position.',
+    muscle_group: 'Back',
+    video_url: 'https://www.youtube.com/watch?v=SALxEARiMkw',
+    image_url: require('../assets/lat-pull-down.jpg'),
+  },
+  {
+    id: 3,
+    name: 'Leg Extension',
+    how_to:
+      'To perform a leg extension, sit on a leg extension machine with your back against the backrest and your knees bent at a 90-degree angle. Extend your legs, pushing the padded bar up with your shins until your legs are fully extended, and then slowly lower the weight back to the starting position while exhaling',
+    muscle_group: 'Leg',
+    video_url: 'https://www.youtube.com/watch?v=ykJmrZ5v0Oo',
+    image_url: require('../assets/leg-extension.jpg'),
+  },
+  {
+    id: 4,
+    name: 'Back Squat',
+    how_to:
+      'To do a back squat, start by standing with your feet shoulder-width apart and a barbell resting on your upper back, held in place by placing your hands on the bar with a wider-than-shoulder-width grip. Bend your knees and hips simultaneously to lower your body, keeping your back straight, and descend until your thighs are parallel to the ground, then push through your heels to stand back up to the starting position.',
+    muscle_group: 'Leg',
+    video_url: 'https://www.youtube.com/watch?v=ykJmrZ5v0Oo',
+    image_url: require('../assets/back-squat.jpg'),
+  },
+  {
+    id: 5,
+    name: 'Tricep Pushdown (V-grip)',
+    how_to:
+      'To perform a tricep pushdown with a v-grip, attach a v-bar handle to a high pulley cable machine. Stand facing the machine, grab the v-grip with your palms facing each other, and with your elbows close to your sides, push the bar down by extending your elbows fully, then slowly release it back up to the starting position.',
+    muscle_group: 'Arm',
+    video_url: 'https://www.youtube.com/watch?v=ykJmrZ5v0Oo',
+    image_url: require('../assets/trice-pushdown-vgrip.jpg'),
+  },
+  {
+    id: 6,
+    name: 'Bench Press',
+    how_to:
+      'To do a bench press, lie on a flat bench with your feet flat on the floor, and grip the barbell with your hands slightly wider than shoulder-width apart. Lower the bar to your chest, then push it back up to the starting position, making sure to keep your back, head, and buttocks in contact with the bench and your elbows at around a 45-degree angle to your torso.',
+    muscle_group: 'Chest',
+    video_url: 'https://www.youtube.com/watch?v=ykJmrZ5v0Oo',
+    image_url: require('../assets/bench-press.jpg'),
+  },
+];
